d951f799c8541c93fb72a055e5742c7231983194
donatasjan5 <donatasjan5@gmail.com>
2023-05-17 13:10:26 +0300

    prisijungimo patvirtinimas ivedus email ir password

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,48 +1,57 @@
-import { Component } from "react";
-import Header1 from "./Header1";
-import styled from "styled-components";
-import Input from "./Input";
-import BlueButton from "./BlueButton";
-import axios from "axios";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Input from './Input';
+import BlueButton from './BlueButton';
 
 const Container = styled.div`
   padding: 30px 20px;
 `;
 
-class LoginPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-    };
-  }
-  login() {
-    axios.post("http://localhost:3030/login", {
-      email: this.state.email,
-      password: this.state.password,
-    });
-  }
-  render() {
-    return (
-      <Container>
-        <Header1 style={{ marginBottom: "20px" }}>Login</Header1>
-        <Input
-          placeholder={"email"}
-          type="email"
-          value={this.state.email}
-          onChange={(ev) => this.setState({ email: ev.target.value })}
-        />
-        <Input
-          placeholder={"password"}
-          type="password"
-          value={this.state.password}
-          onChange={(ev) => this.setState({ password: ev.target.value })}
-        />
-        <BlueButton onClick={() => this.login()}>Login</BlueButton>
-      </Container>
-    );
-  }
-}
+const LoginPage = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Patikrinkite prisijungimo duomenis ir nustatykite isLoggedIn būseną
+    if (email === 'donatasjan5@gmail.com' && password === 'password') {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  };
+
+  const handleLogout = () => {
+    // Atsijungimo veiksmai
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <Container>
+      {isLoggedIn ? (
+        <div>
+          <p>Jūs esate prisijungęs.</p>
+          <button onClick={handleLogout}>Atsijungti</button>
+        </div>
+      ) : (
+        <div>
+          <Input
+            placeholder={'email'}
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder={'password'}
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <BlueButton onClick={handleLogin}>Login</BlueButton>
+        </div>
+      )}
+    </Container>
+  );
+};
 
 export default LoginPage;
