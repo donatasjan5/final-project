083aed7f8872e17729af900d26bac5ef83ebaf04
donatasjan5 <donatasjan5@gmail.com>
2023-05-19 10:43:05 +0300

    komentaro edit ir delete

--- src/components/AskPage.jsx
+++ src/components/AskPage.jsx
@@ -30,19 +30,8 @@ const QuestionContainer = styled.div`
   }
 `;
 
-const EditButton = styled.button`
-  background-color: #777;
-  color: white;
-  border: none;
-  border-radius: 3px;
-  padding: 8px 16px;
-  margin-top: 10px;
-  margin-left: 10px;
-  cursor: pointer;
-`;
-
-const DeleteButton = styled.button`
-  background-color: red;
+const ActionButton = styled.button`
+  background-color: ${(props) => (props.delete ? "red" : "#777")};
   color: white;
   border: none;
   border-radius: 3px;
@@ -76,7 +65,8 @@ export default function AskPage() {
   const [questionBody, setQuestionBody] = useState("");
   const [questions, setQuestions] = useState([]);
   const [comment, setComment] = useState("");
-  const [editedIndexes, setEditedIndexes] = useState([]);
+  const [editedIndex, setEditedIndex] = useState(null);
+  const [editedComment, setEditedComment] = useState("");
 
   const addQuestion = () => {
     const newQuestion = {
@@ -90,29 +80,48 @@ export default function AskPage() {
     setQuestionBody("");
   };
 
-  const editQuestion = (index) => {
+  const editItem = (index) => {
+    setEditedIndex(index);
     const editedQuestion = questions[index];
     setQuestionTitle(editedQuestion.title);
     setQuestionBody(editedQuestion.body);
-
-    const updatedQuestions = [...questions];
-    updatedQuestions.splice(index, 1);
-    setQuestions(updatedQuestions);
-
-    setEditedIndexes([...editedIndexes, index]);
   };
 
-  const deleteQuestion = (index) => {
+  const deleteItem = (index) => {
     const updatedQuestions = [...questions];
     updatedQuestions.splice(index, 1);
     setQuestions(updatedQuestions);
   };
 
-  const addComment = (questionIndex) => {
+  const addItemComment = (questionIndex) => {
     const updatedQuestions = [...questions];
     updatedQuestions[questionIndex].comments.push(comment);
     setQuestions(updatedQuestions);
     setComment("");
+  };
+
+  const editComment = (questionIndex, commentIndex) => {
+    setEditedIndex(questionIndex + "_" + commentIndex);
+    setEditedComment(questions[questionIndex].comments[commentIndex]);
+  };
+
+  const saveEditedComment = (questionIndex, commentIndex) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions[questionIndex].comments[commentIndex] = editedComment;
+    setQuestions(updatedQuestions);
+    setEditedIndex(null);
+    setEditedComment("");
+  };
+
+  const cancelEditComment = () => {
+    setEditedIndex(null);
+    setEditedComment("");
+  };
+
+  const deleteComment = (questionIndex, commentIndex) => {
+    const updatedQuestions = [...questions];
+    updatedQuestions[questionIndex].comments.splice(commentIndex, 1);
+    setQuestions(updatedQuestions);
   };
 
   return (
@@ -138,15 +147,15 @@ export default function AskPage() {
             <QuestionContainer key={questionIndex}>
               <h3>
                 {q.title}
-                {editedIndexes.includes(questionIndex) && " (edited)"}
+                {editedIndex === questionIndex && " (edited)"}
               </h3>
               <p>{q.body}</p>
-              <EditButton onClick={() => editQuestion(questionIndex)}>
+              <ActionButton onClick={() => editItem(questionIndex)}>
                 Edit
-              </EditButton>
-              <DeleteButton onClick={() => deleteQuestion(questionIndex)}>
+              </ActionButton>
+              <ActionButton delete onClick={() => deleteItem(questionIndex)}>
                 Delete
-              </DeleteButton>
+              </ActionButton>
               <CommentInput
                 type="text"
                 value={comment}
@@ -154,7 +163,7 @@ export default function AskPage() {
                 placeholder="Add a comment"
               />
               <BlueButton
-                onClick={() => addComment(questionIndex)}
+                onClick={() => addItemComment(questionIndex)}
                 disabled={!comment}
               >
                 Add Comment
@@ -164,7 +173,45 @@ export default function AskPage() {
                   <h4>Comments:</h4>
                   {q.comments.map((comment, commentIndex) => (
                     <CommentContainer key={commentIndex}>
-                      <p>{comment}</p>
+                      {editedIndex === questionIndex + "_" + commentIndex ? (
+                        <>
+                          <CommentInput
+                            type="text"
+                            value={editedComment}
+                            onChange={(e) => setEditedComment(e.target.value)}
+                            placeholder="Edit the comment"
+                          />
+                          <ActionButton
+                            onClick={() =>
+                              saveEditedComment(questionIndex, commentIndex)
+                            }
+                          >
+                            Save
+                          </ActionButton>
+                          <ActionButton delete onClick={cancelEditComment}>
+                            Cancel
+                          </ActionButton>
+                        </>
+                      ) : (
+                        <>
+                          <p>{comment}</p>
+                          <ActionButton
+                            onClick={() =>
+                              editComment(questionIndex, commentIndex)
+                            }
+                          >
+                            Edit
+                          </ActionButton>
+                          <ActionButton
+                            delete
+                            onClick={() =>
+                              deleteComment(questionIndex, commentIndex)
+                            }
+                          >
+                            Delete
+                          </ActionButton>
+                        </>
+                      )}
                     </CommentContainer>
                   ))}
                 </div>
